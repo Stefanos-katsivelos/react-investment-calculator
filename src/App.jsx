c3e5cb4322bb5a3a578a0c0a225638fbebe5d3e8
Stefanos-katsivelos <stef.katsivelos@gmail.com>
2024-08-22 17:21:01 +0300

    fixing a bug related to duration value when userInput is minus

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ function App() {
     duration: 10,
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
   /*
     The plus oparator ( +newValue ) fix the bug that calculatorInvestmentResults,
     it cannot resolve with the concatination strings, therefore the calculated numbers
@@ -29,7 +31,10 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
